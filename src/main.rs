3409bc3ca2533d4daa6a97bec9d55cdcae99378f
Henry Barreto <me@henrybarreto.dev>
2023-07-22 10:11:28 -0300

    feat!: add bevy's pixels plugin to draw emulator buffer
    NOTICE: From this commit, I have adopted the conventional commits
    pattern

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,42 @@
-use minifb::{Key, Window, WindowOptions};
 use std::cell::Cell;
+
+use bevy::prelude::*;
+use bevy_pixels::prelude::*;
 
 mod emulator;
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let rom = &args[1];
+fn draw(mut wrapper_query: Query<&mut PixelsWrapper>) {
+    // Query the `PixelsWrapper` component that owns an instance of `Pixels` for the given window.
+    let Ok(mut wrapper) = wrapper_query.get_single_mut() else { return };
 
-    let emu = emulator::Emualtor {};
+    wrapper.pixels.resize_buffer(240, 160).unwrap();
+    // Get a mutable slice for the pixel buffer.
+    let frame: &mut [u8] = wrapper.pixels.frame_mut();
 
-    emu.init();
+    let (video, _) = EMU.run();
 
-    emu.load_rom(rom);
+    match video {
+        Some(buffer) => unsafe {
+            let slice = std::slice::from_raw_parts(buffer.as_ptr() as *const u8, buffer.len() * 4);
 
-    let width = 240;
-    let height = 160;
-
-    let mut window = Window::new(
-        "Test - ESC to exit",
-        width,
-        height,
-        WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
-
-    // Limit to max ~30 fps update rate
-    // window.limit_update_rate(Some(std::time::Duration::from_micros(33300)));
-
-    // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-
-    /*let mut fps_timer = std::time::Instant::now();
-    let mut fps_counter = 0;*/
-
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        /*fps_counter += 1;
-        let elapsed = fps_timer.elapsed();
-        if elapsed >= std::time::Duration::from_secs(1) {
-            let fps = fps_counter as f64 / elapsed.as_secs_f64();
-            window.set_title(&format!("Rust Game (FPS: {:.2})", fps));
-            fps_counter = 0;
-            fps_timer = std::time::Instant::now();
-        }*/
-
-        //   BY SELECT START   UDLR AXLR L2 R2 L3 R3
-        //  [00 0      0       0000 0000 0  0  0  0]
-        let mut buttons = vec![0; 16];
-
-        let mini_fb_keys = window.get_keys();
-        for key in mini_fb_keys {
-            match key {
-                Key::Enter => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_START as usize] = 1;
-                }
-                Key::Right => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_RIGHT as usize] = 1;
-                }
-                Key::Left => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_LEFT as usize] = 1;
-                }
-                Key::Up => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_UP as usize] = 1;
-                }
-                Key::Down => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_DOWN as usize] = 1;
-                }
-                // VIM keys.
-                Key::L => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_RIGHT as usize] = 1;
-                }
-                Key::H => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_LEFT as usize] = 1;
-                }
-                Key::K => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_UP as usize] = 1;
-                }
-                Key::J => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_DOWN as usize] = 1;
-                }
-                Key::A => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_A as usize] = 1;
-                }
-                Key::S => {
-                    buttons[emulator::RETRO_DEVICE_ID_JOYPAD_B as usize] = 1;
-                }
-                Key::F1 => {
-                    println!("Save State");
-                    emu.save_state();
-                }
-                Key::F2 => {
-                    println!("Load State");
-                    emu.load_state();
-                }
-                _ => {
-                    println!("Unhandled Key Pressed: {:?}", key);
-                }
-            }
-        }
-
-        emu.push_buttons(buttons);
-
-        let (video, _) = emu.run();
-
-        match video {
-            Some(buffer) => {
-                window.update_with_buffer(&buffer, width, height).unwrap();
-            }
-            None => {
-                println!("We don't have a buffer to display");
-            }
+            frame.copy_from_slice(slice);
+        },
+        None => {
+            println!("We don't have a buffer to display");
         }
     }
+}
+
+static EMU: emulator::Emualtor = emulator::Emualtor {};
+
+fn main() {
+    EMU.init();
+    EMU.load_rom("test.gba");
+
+    App::new()
+        .add_plugins((DefaultPlugins, PixelsPlugin::default()))
+        // Add systems that draw to the buffer to `Draw` schedule
+        // to ensure they are rendered in the current frame.
+        .add_systems(Draw, draw)
+        .run();
 }
